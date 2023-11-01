b687071b9567fb1b05693ca71668db1b13913914
Stheven <sthevenalvess@gmail.com>
2023-11-01 16:57:49 -0300

    Lógica de tabela e inserção de dados

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,23 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        //Um ApplicationDbContext apenas para leitura, private tem que ter um anderline no nome
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+    }
+}
