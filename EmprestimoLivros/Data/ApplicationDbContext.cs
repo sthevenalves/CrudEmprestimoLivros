28f804cb91e0044dcf9643da7627fb7352686953
Stheven <sthevenalvess@gmail.com>
2023-11-01 16:28:09 -0300

    Criando e conectando com o SQL Server

---
+++ EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //criando tabela
+        public DbSet<EmprestimoModel> Emprestimos { get; set; }
+    }
+}
+/*
+ * DbContext auxilia no processo de conexão com o Banco de Dados, é necessario 
+ * o using Microsoft.EntityFrameworkCore;
+ */
+/*
+ *  base(options) significa que ele ira pegar as opções do DbContext
+ */
--- EmprestimoLivros/Models/EmprestimoModel.cs
+++ EmprestimoLivros/Models/EmprestimoModel.cs
@@ -2,5 +2,10 @@
 {
     public class EmprestimoModel
     {
+        public int Id { get; set; }//coluna id na BD
+        public String Recebedor { get; set; }
+        public string Fornecedor { get; set;}
+        public string LivroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao {  get; set; } = DateTime.Now;
     }
 }
