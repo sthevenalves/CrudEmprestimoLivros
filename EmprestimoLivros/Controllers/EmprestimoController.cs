a7b443769e73a9c086b551f00a69e3df1cd10876
Stheven <sthevenalvess@gmail.com>
2023-11-05 15:01:09 -0300

    Botstrap do site e codificando funcionalidades

--- EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -19,5 +19,87 @@ namespace EmprestimoLivros.Controllers
             IEnumerable<EmprestimoModel> emprestimos = _db.Emprestimos;
             return View(emprestimos);
         }
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+
+            return View(); 
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            /*
+             *Equivale ao select * from [dbo].[Emprestimos] where id? = id 
+            */
+            EmprestimoModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+                return View(emprestimo);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimoModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if(emprestimo == null){
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+        [HttpPost]
+        public IActionResult Excluir(EmprestimoModel emprestimo)
+        {
+            if(emprestimo == null){
+                return NotFound();
+            }
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimoModel emprestimos)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);//adicionando cadastros
+                _db.SaveChanges();//salvando as mudanças
+
+                return RedirectToAction("Index");//rediciona o usuario para a action index
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimoModel emprestimo)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(emprestimo);
+        }
+
     }
 }
--- EmprestimoLivros/Models/EmprestimoModel.cs
+++ EmprestimoLivros/Models/EmprestimoModel.cs
@@ -1,11 +1,21 @@
-﻿namespace EmprestimoLivros.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models
 {
     public class EmprestimoModel
     {
+
         public int Id { get; set; }//coluna id na BD
+
+        [Required(ErrorMessage = "Digite o nome do Recebedor")]
         public String Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Fornecedor")]
         public string Fornecedor { get; set;}
+
+        [Required(ErrorMessage = "Digite o nome do Livro")]
         public string LivroEmprestado { get; set; }
+
         public DateTime dataUltimaAtualizacao {  get; set; } = DateTime.Now;
     }
 }
